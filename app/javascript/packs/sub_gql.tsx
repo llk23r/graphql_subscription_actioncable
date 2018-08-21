2a54f824ce3e025680fac8d87a7a8654d22ef349
ac <ac@domain.local>
2018-08-21 18:29:01 +0900

    test gql query

--- app/javascript/packs/sub_gql.tsx
+++ app/javascript/packs/sub_gql.tsx
@@ -1,9 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import ApolloClient from "apollo-boost"
+const client = new ApolloClient()
+
+import gql from "graphql-tag"
+import { ApolloProvider, Query } from "react-apollo"
+const TestQuery = gql`
+  {
+    testField
+  }
+`
+
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <div>typescript</div>,
+    <ApolloProvider client={client}>
+      <Query query={TestQuery}>
+        {({ data, loading }) => {
+          if (loading) return <p>loading</p>
+          return <div>{data.testField}</div>
+        }}
+      </Query>
+    </ApolloProvider>,
     document.body.appendChild(document.createElement("div"))
   )
 })
