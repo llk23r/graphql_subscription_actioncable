309b0e8dd1014583104c109a9638c905f94f8a39
ac <ac@domain.local>
2018-08-21 18:15:35 +0900

    remove sample files

--- app/javascript/packs/hello_typescript.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <div>typescript</div>,
-    document.body.appendChild(document.createElement("div"))
-  )
-})
