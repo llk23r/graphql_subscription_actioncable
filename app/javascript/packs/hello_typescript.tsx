3dcaffb53ee88a46bd2153d3375b6c5fe41686dc
ac <ac@domain.local>
2018-08-21 18:12:17 +0900

    make sure typescript works

--- app/javascript/packs/hello_typescript.tsx
+++ app/javascript/packs/hello_typescript.tsx
@@ -1,4 +1,9 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_typescript' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb.
+import React from "react"
+import ReactDOM from "react-dom"
 
-console.log('Hello world from typescript');
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <div>typescript</div>,
+    document.body.appendChild(document.createElement("div"))
+  )
+})
