9316c96146ab5b4bb298110b7a8e93bf0a9e7b90
ac <ac@domain.local>
2018-08-21 18:17:28 +0900

    prepare an endpoint

---
+++ app/javascript/packs/sub_gql.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <div>typescript</div>,
+    document.body.appendChild(document.createElement("div"))
+  )
+})
