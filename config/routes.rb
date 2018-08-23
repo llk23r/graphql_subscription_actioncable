760427ef21bcb1d740c4e2eb0758edd8357951d1
ac <ac@domain.local>
2018-08-23 11:54:28 +0900

    add sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
