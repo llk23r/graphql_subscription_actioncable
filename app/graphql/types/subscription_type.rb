55f5069e75c4c06670d5111b0360f08e6d297c16
ac <ac@domain.local>
2018-08-29 10:14:08 +0900

    add a workaround to determine if a call is an initial one

--- app/graphql/types/subscription_type.rb
+++ app/graphql/types/subscription_type.rb
@@ -5,7 +5,13 @@ module Types
       description: "subscription test"
 
     def test_sub
-      TestWorker.perform_async
+      TestWorker.perform_async if initial_call?
     end
+
+    private
+
+      def initial_call?
+        context.to_h.keys.exclude?(:subscription_id)
+      end
   end
 end
